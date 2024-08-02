d7d0939f02450b6e893435bb4b7397f06bd8aac1
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-08-02 18:43:10 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WeltecEveningBatch
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeltecEveningBatch
+{
+    public class EmployeeDbContext: DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
